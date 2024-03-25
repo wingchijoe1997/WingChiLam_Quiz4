07a2cccb1bd70d00206e2439d1961d351e5e3c65
Wing Chi Lam <wlam86@my.centennialcollege.ca>
2024-03-25 14:03:36 -0400

    Add project files.

---
+++ WingChiLam_Quiz4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WingChiLam_Quiz4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
